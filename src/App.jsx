b034828d9ca76474c570e1268615b79a8a420e3d
Fiyinfoluwa John Ajala <johnajala204@gmail.com>
2022-10-29 09:24:04 +0100

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// import { useState } from 'react'
+import Nav from './components/Nav'
+import './App.css'
+import Header from './components/Header'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <main>
+      <Nav />
+      <Header />
+    </main>
+  )
+}
+
+export default App
+
+
+// font-[montserrat]
+// font-[chillax-variable]
---
+++ src/components/Nav.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Nav() {
+  return (
+      <nav className='w-full bg-green-800'>
+          <div className='font-[chillax-variable] font-medium text-lg bg-red-500'> <span className='block leading-3'>Meta</span> <span>Space</span> </div>
+
+          <section>
+              
+          </section>
+
+          <section className='hidden'>
+              <h1>Desktop Menu</h1>              
+          </section>
+
+      </nav>
+  )
+}
